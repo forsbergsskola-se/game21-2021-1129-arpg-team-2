7eb0e8bc2a0ab4b28f4fe38f17920c1c9a5f936d
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2021-11-29 15:53:48 +0100

    Update ObjectJO.cs

--- Assets/Programmers/JO/Scripts/Object/ObjectJO.cs
+++ Assets/Programmers/JO/Scripts/Object/ObjectJO.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class ObjectJO : MonoBehaviour, IDamageableJO
 {
     [SerializeField] IntegerValue objectHealth;
+    static int weaponDam = 10;
 
     void IDamageableJO.TakeDamage(int WeaponDamage) {
         objectHealth.Int -= WeaponDamage;
+    }
+
+    void main() {
+        //IDamageableJO.TakeDamage(weaponDam);
     }
 }
