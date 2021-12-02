6a7d44b8bea4bd8dd8f84397423e32b3718a1625
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2021-12-02 13:54:58 +0100

    Continued work on rat spawner + documentation for thought process of how it should work

--- Assets/Programmers/JO/Scripts/SpawnerJO.cs
+++ Assets/Programmers/JO/Scripts/SpawnerJO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,26 @@ using UnityEngine;
 public class SpawnerJO : MonoBehaviour {
     [SerializeField] private GameObject ratPrefab;
     [SerializeField] private Vector3Value[] spawnPositions;
+    [SerializeField] private FloatValue spawnInterval;
+    private GameObject[] spawnedRats;
+
+    private void Start()
+    {
+        for (int i = 0; i < spawnPositions.Length; i++)
+        {
+            GameObject spawnedRat = Instantiate(ratPrefab, spawnPositions[i].Vector3, Quaternion.identity);
+            spawnedRats[i] = spawnedRat;
+            
+            //Want to subscribe to action of rat death
+            //Want to call respawn for dead rat after set interval
+            //Will add +1 to amount of killed rat
+            //Door to corresponding room subscribes to amount of killed rats
+            //When killed rats matches kill criteria door opens
+        }
+    }
+
+    private IEnumerator Respawn() {
+        yield return new WaitForSeconds(spawnInterval.InitialValue);
+        
+    }
 }
