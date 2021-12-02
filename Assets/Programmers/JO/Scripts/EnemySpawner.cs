43dbb281130e65802d0cb8eb58239234ba166b63
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2021-12-02 23:06:15 +0100

    Gate unlocks when all enemies in room is killed
    Only implemented in JO scene

--- Assets/Programmers/JO/Scripts/EnemySpawner.cs
+++ Assets/Programmers/JO/Scripts/EnemySpawner.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 
 [RequireComponent(typeof(EnemyPooler))]
 public class EnemySpawner : MonoBehaviour {
+    public Action<int> OnEnemyDeath;
     [SerializeField] private SpawnPoint[] spawnPositions;
     [SerializeField] private FloatValue spawnInterval;
     [SerializeField] private EnemyJO enemyPrefab;
     private EnemyPooler enemyPool;
     private List<GameObject> _spawnedObjects;
     private int deathCount;
+    public int SpawnPointsCount {
+        get { return spawnPositions.Length; }
+    }
+    
     private void Start() {
         enemyPool = GetComponent<EnemyPooler>();
         enemyPool.Setup(spawnPositions.Length, enemyPrefab);
@@ -44,6 +49,7 @@ public class EnemySpawner : MonoBehaviour {
 
     private void EnemyDied(EnemyJO enemy) {
         deathCount++;
+        OnEnemyDeath?.Invoke(deathCount);
         StartCoroutine(Respawn(enemy.SpawnIndex));
     }
 }
---
+++ Assets/Programmers/JO/Scripts/GateJO.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class GateJO : MonoBehaviour {
+    [SerializeField] private EnemySpawner enemySpawner;
+    private Animator doorAnimation;
+    private bool isLocked = true;
+    private int initialEnemiesInRoom;
+    
+    private void Start()
+    {
+        doorAnimation = GetComponent<Animator>();
+        enemySpawner.OnEnemyDeath += EnemyDied;
+        initialEnemiesInRoom = enemySpawner.SpawnPointsCount;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!isLocked)
+        {
+            doorAnimation.SetBool("isOpening", true);
+        }
+    }
+
+    private void EnemyDied(int deathCount)
+    {
+        if (deathCount >= initialEnemiesInRoom)
+        {
+            isLocked = false;
+        }
+    }
+}
