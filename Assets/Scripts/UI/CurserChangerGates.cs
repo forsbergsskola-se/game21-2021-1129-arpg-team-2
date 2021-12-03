5cac73fba11b2c92ab7b2af885a3a25c88cce7f8
Armin-AF <armin.afazeli@student.forsbergsskola.se>
2021-12-03 16:01:48 +0100

    Gate curser changes

--- Assets/Scripts/UI/CurserChangerGates.cs
+++ Assets/Scripts/UI/CurserChangerGates.cs
@@ -6,7 +6,7 @@ public class CurserChangerGates : MonoBehaviour
 {
     public Texture2D cursorBase;
     public Texture2D cursorLocked;
-    public Texture2D cursorUnklocked;
+    public Texture2D cursorUnlocked;
     public CursorMode cursorMode = CursorMode.ForceSoftware;
     public Vector2 hotSpot = Vector2.zero;
     Animator anim;
@@ -20,13 +20,14 @@ public class CurserChangerGates : MonoBehaviour
     
     void OnMouseEnter()
     {
-        if (GateIsClosed)
+        Debug.Log("Gate is Closed?"+ GateIsClosed.BoolValue);
+        if (GateIsClosed.BoolValue)
         {
             Cursor.SetCursor(cursorLocked, hotSpot, cursorMode);
         }
         else
         {
-            Cursor.SetCursor(cursorUnklocked, hotSpot, cursorMode);
+            Cursor.SetCursor(cursorUnlocked, hotSpot, cursorMode);
         }
     }    
     void OnMouseExit()
