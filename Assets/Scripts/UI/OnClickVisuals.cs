e1d0b6ed08d57cf1c091296dbfd5c8f069a6d3c5
Armin-AF <armin.afazeli@student.forsbergsskola.se>
2021-12-03 12:07:55 +0100

    Bug Fixes

--- Assets/Scripts/UI/OnClickVisuals.cs
+++ Assets/Scripts/UI/OnClickVisuals.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,7 +13,21 @@ public class OnClickVisuals : MonoBehaviour
     public AudioSource Hammer;
     public AudioSource Sword;
     public AudioSource Gate;
+    public Transform prefabInitialPosition;
+    private GameObject instantiatedValid;
+    private GameObject instantiatedInvalid;
     
+
+    private void Start()
+    {
+        
+        instantiatedValid = Instantiate(myPrefabValid, prefabInitialPosition.position, myPrefabValid.transform.rotation);
+        instantiatedInvalid = Instantiate(myPrefabInvalid, prefabInitialPosition.position, myPrefabInvalid.transform.rotation);
+        prefabInitialPosition.position = Vector3.zero;
+        prefabInitialPosition.rotation = instantiatedValid.transform.rotation;
+
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -22,8 +37,9 @@ public class OnClickVisuals : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit) && CheckDestination(hit.transform.position) && !hit.transform.CompareTag("Destructible"))
             {
-                Instantiate(myPrefabValid, hit.point, Quaternion.identity);
                 ValidMove.Play();
+                instantiatedValid.transform.SetPositionAndRotation(hit.point, prefabInitialPosition.rotation);
+                instantiatedValid.GetComponent<Animation>().Play();
 
             }
             else if (hit.transform.CompareTag("Destructible"))
@@ -40,7 +56,8 @@ public class OnClickVisuals : MonoBehaviour
             }
             else
             {
-                Instantiate(myPrefabInvalid, hit.point, Quaternion.identity);
+                instantiatedInvalid.transform.SetPositionAndRotation(hit.point, prefabInitialPosition.rotation);
+                instantiatedInvalid.GetComponent<Animation>().Play();
                 InvalidMove.Play();
             }
         }
