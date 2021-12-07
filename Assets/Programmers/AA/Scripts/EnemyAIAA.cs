ec4de3bfd350178639ee5dc71e7e9c8a251c29fd
Armin-AF <armin.afazeli@student.forsbergsskola.se>
2021-12-07 10:16:21 +0100

    Enemy Ai Movement Updated. Code Clean up

--- Assets/Programmers/AA/Scripts/EnemyAIAA.cs
+++ Assets/Programmers/AA/Scripts/EnemyAIAA.cs
@@ -7,14 +7,10 @@ using Random = UnityEngine.Random;
 
 public class EnemyAIAA : MonoBehaviour
 {
-    public NavMeshAgent agent;
-
     [SerializeField] private Vector3Value playerPosition;
-
+    public NavMeshAgent agent;
     public LayerMask Player, Ground;
-
-    public float health;
-
+   
     //Patrolling
     public Vector3 walkPoint;
     bool walkPointSet;
@@ -23,31 +19,18 @@ public class EnemyAIAA : MonoBehaviour
     //Attacking
     public float timeBetweenAttacks;
     bool alreadyAttacked;
-    //public GameObject projectile;
 
     //States
     public float sightRange, attackRange;
     public bool playerInSightRange, playerInAttackRange;
     
-    private enum State
-    {
-        
-    }
-
-    private State state;
-
-    private void Awake()
-    {
-        //playerPosition.Vector3 = GameObject.Find("PlayerObj").transform.position;
-        //agent = GetComponent<NavMeshAgent>();
-    }
-
+    
     private void Update()
     {
         //Check for sight and attack range
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, Player);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, Player);
-
+        
         if (!playerInSightRange && !playerInAttackRange) Patrolling();
         if (playerInSightRange && !playerInAttackRange) ChasePlayer();
         if (playerInAttackRange && playerInSightRange) AttackPlayer();
@@ -80,6 +63,7 @@ public class EnemyAIAA : MonoBehaviour
 
     private void ChasePlayer()
     {
+        Debug.Log("Enemy found the player!");
         agent.SetDestination(playerPosition.Vector3);
     }
 
@@ -92,12 +76,6 @@ public class EnemyAIAA : MonoBehaviour
 
         if (!alreadyAttacked)
         {
-            //Attack code here
-            //Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-            //rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
-            //rb.AddForce(transform.up * 8f, ForceMode.Impulse);
-            //End of attack code
-
             Debug.Log("Attack");
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
@@ -106,24 +84,5 @@ public class EnemyAIAA : MonoBehaviour
     private void ResetAttack()
     {
         alreadyAttacked = false;
-    }
-
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-
-        if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
-    }
-    private void DestroyEnemy()
-    {
-        Destroy(gameObject);
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, attackRange);
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
