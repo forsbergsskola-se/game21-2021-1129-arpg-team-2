d88bfe575b87cba88d41f86ed85cb798dac2d3b7
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2021-12-07 11:50:50 +0100

    BillboardAndHealthBarGoesDown

---
+++ Assets/Programmers/MT/Scripts/HealthBarController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+   public Image healthBar;
+   private  Health health;
+
+
+   private void Awake()
+   {
+      health = GetComponentInParent<Health>();
+   }
+
+   private void LateUpdate()
+   {
+      healthBar.fillAmount = health.CurrentHealth.RuntimeValue / health.CurrentHealth.InitialValue;
+   }
+   
+   
+}
