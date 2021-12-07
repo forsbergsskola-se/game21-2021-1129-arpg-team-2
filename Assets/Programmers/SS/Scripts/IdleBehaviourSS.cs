21c95ca3b08109febff892afd26aa237ae84ad77
Sara SABER <sarasaberdev@gmail.com>
2021-12-07 15:03:25 +0300

    Add Idle behaviour

--- Assets/Programmers/SS/Scripts/IdleBehaviourSS.cs
+++ Assets/Programmers/SS/Scripts/IdleBehaviourSS.cs
@@ -2,33 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChaseBehaviourSS : StateMachineBehaviour
+public class IdleBehaviourSS : StateMachineBehaviour
 {
-
-    private Transform playerPos;
-    public float chaseSpeed;
-
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        playerPos=GameObject.FindGameObjectWithTag("Player").transform;
+        
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.transform.position = Vector3.MoveTowards(animator.transform.position, playerPos.position, chaseSpeed);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            animator.SetBool("isFollowing", false);
+            animator.SetBool("isFollowing", true);
         }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        
-    }
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
