0a2b33b9bec032b633ae05c3700e5254540fcb5a
InaSLew <ina.shenlewander@gmail.com>
2021-12-08 17:09:35 +0100

    Screen fades to black upon player defeated

--- Assets/Scripts/DarkenScreen.cs
+++ Assets/Scripts/DarkenScreen.cs
@@ -1,22 +1,48 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DarkenScreen : MonoBehaviour
 {
-    [SerializeField] private float distanceFromCamera;
-    private Camera cam;
-
-    private void Awake()
-    {
-        cam = Camera.main;
-    }
+    [SerializeField] private float fadeSpeed;
+    private Image imageBlack;
 
     public void OnPlayerDefeated()
     {
-        transform.position = cam.transform.position + cam.transform.forward * distanceFromCamera;
+        imageBlack = GetComponentInChildren<Image>();
+        StartCoroutine(FadeInBlack());
     }
 
-    public void OnPlayerNotDefeated()
+    private IEnumerator FadeInBlack(bool fadeToBlack = true)
     {
-        // move the object outside camera view
+        var imgColor = imageBlack.color;
+        float fadeAmount;
+
+        if (fadeToBlack)
+        {
+            Debug.Log("Inside fadeToBlack, image color alpha: " + imageBlack.color.a);
+            while (imageBlack.color.a < 1)
+            {
+                Debug.Log("fading to black coroutine firing");
+                fadeAmount = imgColor.a + (fadeSpeed * Time.deltaTime);
+                imgColor = new Color(imgColor.r, imgColor.g, imgColor.b, fadeAmount);
+                imageBlack.color = imgColor;
+                yield return null;
+            }
+        }
+        else
+        {
+            while (imageBlack.color.a > 0)
+            {
+                fadeAmount = imgColor.a - (fadeSpeed * Time.deltaTime);
+                imgColor = new Color(imgColor.r, imgColor.g, imgColor.b, fadeAmount);
+                imageBlack.color = imgColor;
+                yield return null;
+            }
+        }
+        yield return new WaitForEndOfFrame();
     }
 }
