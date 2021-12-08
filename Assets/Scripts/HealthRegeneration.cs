4a0d8a7191662265185cbc8735fdf21b6ec41f29
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-12-08 13:58:59 +0100

    Health regenerates when reaches 0

--- Assets/Scripts/HealthRegeneration.cs
+++ Assets/Scripts/HealthRegeneration.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class HealthRegeneration : MonoBehaviour
 {
-    [SerializeField] private GameEvent entityDeath;
     [SerializeField] private FloatValue playerHealth;
-    [SerializeField] private FloatValue regenerationRate;
+    [SerializeField] private float regenerationRate;
+
+    public void RegenerateHealth()
+    {
+        playerHealth.RuntimeValue = playerHealth.InitialValue;
+    }
 
 }
