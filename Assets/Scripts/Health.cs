1a811db38999c6ee2347f6596aeb1bc8747394f9
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-12-08 11:36:33 +0100

    Health Component Takes external ScriptableObject

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,9 +2,8 @@ using UnityEngine;
 
 public class Health : MonoBehaviour, IDamageable
 {
-    [SerializeField] private float maxHealth;
     public GameEvent entityDeath;
-    private FloatValue currentHealth;
+    [SerializeField] private FloatValue currentHealth;
     public FloatValue CurrentHealth { get => currentHealth; set => currentHealth = value; }
     
     // Flash red when taking damage
@@ -19,10 +18,7 @@ public class Health : MonoBehaviour, IDamageable
     {
         render = GetComponent<Renderer>();
         defaultColor = render.material.color;
-        currentHealth = ScriptableObject.CreateInstance<FloatValue>();
-        currentHealth.RuntimeValue = maxHealth;
-        currentHealth.InitialValue = maxHealth;
-
+        
         // GameObject HealthBar = GameObject.Find("EnemyHealthBar");
         // HealthBar.SetActive(false);
     }
@@ -41,12 +37,12 @@ public class Health : MonoBehaviour, IDamageable
     public void TakeDamage(float damage)
     {
         FlashRed();
-        CurrentHealth.RuntimeValue -= damage;
+        currentHealth.RuntimeValue -= damage;
 
         Debug.Log("entityDeath event: " + entityDeath);
         
-        if (CurrentHealth.RuntimeValue <= 0f) entityDeath.Raise();
-        Debug.Log(this.gameObject + ": " + CurrentHealth);
+        if (currentHealth.RuntimeValue <= 0f) entityDeath.Raise();
+        Debug.Log(this.gameObject + ": " + currentHealth.RuntimeValue);
     }
     
     // (Hopefully) temporary
