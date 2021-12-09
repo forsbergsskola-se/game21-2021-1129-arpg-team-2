b37d73423a540cb3d99bc63884a3b48385c7c5ad
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-12-09 13:51:02 +0100

    PlayerAttack is no longer checking for null event

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -49,7 +49,7 @@ public class PlayerAttack : MonoBehaviour, IAttack
     {
         // if (thisTarget == null) return;
         attackSound.Play();
-        if(onPlayerAttack != null) onPlayerAttack.Raise();
+        onPlayerAttack.Raise();
         thisTarget.TakeDamage(BasePower.RuntimeValue);
     }
 
