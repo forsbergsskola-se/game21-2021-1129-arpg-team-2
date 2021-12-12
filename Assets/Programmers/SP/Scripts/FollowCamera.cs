70db0c8280d045cdca54b3074b4d3ff7ad19946f
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-12-12 16:59:40 +0100

    Some tests in my Folder SP

---
+++ Assets/Programmers/SP/Scripts/FollowCamera.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+
+    private void LateUpdate()
+    {
+        transform.position = target.position;
+    }
+}
