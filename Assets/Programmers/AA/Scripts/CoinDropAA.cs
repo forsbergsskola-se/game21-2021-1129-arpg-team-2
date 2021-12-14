84e03e15e9491ec97863b38dfc100acca1950bdc
Armin-AF <armin.afazeli@student.forsbergsskola.se>
2021-12-14 19:53:42 +0100

    Pots have coin drop attribute

--- Assets/Programmers/AA/Scripts/CoinDropAA.cs
+++ Assets/Programmers/AA/Scripts/CoinDropAA.cs
@@ -1,12 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class CoinDropAA : MonoBehaviour
 {
     public GameObject coinPrefab;
-    // Start is called before the first frame update
-
+    
     public void DropCoin()
     {
         Instantiate(coinPrefab, transform.position, Quaternion.identity);
