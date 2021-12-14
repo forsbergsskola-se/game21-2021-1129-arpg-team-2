8fbe571136cf58d2d01859c4d65d60d06750d7dd
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-12-14 10:38:41 +0100

    Added Methods to CharStats
    LevelUp and GainExperience methods

--- Assets/Scripts/SOTemplates/CharStats.cs
+++ Assets/Scripts/SOTemplates/CharStats.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class CharStats : ScriptableObject
 {
+    [Header("Stats")]
     [SerializeField] private float health;
     [SerializeField] private float experience;
     [SerializeField] private float attack;
     [SerializeField] private float attackSpeed;
     [SerializeField] private float defence;
+
+    [Header("Dependencies")] 
+    [SerializeField] private LevelUpChart _levelUpChart;
 
     public float Health => health;
     public float Experience => experience;
@@ -16,6 +20,11 @@ public class CharStats : ScriptableObject
 
     public void LevelUp()
     {
-        
+        health *= _levelUpChart.HealthMultiplicator;
+    }
+
+    public void GainExperience(float xpUnity)
+    {
+        experience += xpUnity;
     }
 }
--- Assets/Scripts/SOTemplates/LevelUpChart.cs
+++ Assets/Scripts/SOTemplates/LevelUpChart.cs
@@ -4,8 +4,14 @@ public class LevelUpChart : ScriptableObject
 {
     [Header("Multiplicators")]
     [SerializeField] private float healthMultiplicator;
-    [SerializeField] private float experienceMultiplicator;
     [SerializeField] private float attackMultiplicator;
     [SerializeField] private float attackSpeedMultiplicator;
     [SerializeField] private float defenceMultiplicator;
+    
+    public float HealthMultiplicator => healthMultiplicator;
+    public float AttackMultiplicator => attackMultiplicator;
+    public float AttackSpeedMultiplicator => attackSpeedMultiplicator;
+    public float DefenceMultiplicator => defenceMultiplicator;
+    
+    
 }
