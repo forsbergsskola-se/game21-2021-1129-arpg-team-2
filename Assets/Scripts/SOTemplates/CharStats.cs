e4fbf188353c0dc06af385943a211365a0284b08
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-12-14 10:21:45 +0100

    Added CharStats ScriptableObjects

---
+++ Assets/Scripts/SOTemplates/CharStats.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CharStats : ScriptableObject
+{
+    [SerializeField] private float health;
+    [SerializeField] private float experience;
+    [SerializeField] private float attack;
+    [SerializeField] private float attackSpeed;
+    [SerializeField] private float defence;
+
+    public float Health => health;
+    public float Attack => attack;
+    public float AttackSpeed => attackSpeed;
+    public float Defence => defence;
+
+    public void LevelUp()
+    {
+        
+    }
+}
