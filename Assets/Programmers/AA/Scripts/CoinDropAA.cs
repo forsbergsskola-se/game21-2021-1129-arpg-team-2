348efc4fdd9d941ca705032ec48dc7528a94f322
Armin-AF <armin.afazeli@student.forsbergsskola.se>
2021-12-14 19:27:59 +0100

    Coins after rat death + coin counter UI in the Main Scene

--- Assets/Programmers/AA/Scripts/CoinDropAA.cs
+++ Assets/Programmers/AA/Scripts/CoinDropAA.cs
@@ -6,16 +6,7 @@ public class CoinDropAA : MonoBehaviour
 {
     public GameObject coinPrefab;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void DropCoin()
     {
         Instantiate(coinPrefab, transform.position, Quaternion.identity);
