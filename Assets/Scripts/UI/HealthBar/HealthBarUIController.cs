dc0af010a30a04cdaf41fd71cbea84e0a7f78527
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2021-12-15 15:41:35 +0100

    Health bar fix

--- Assets/Scripts/UI/HealthBar/HealthBarUIController.cs
+++ Assets/Scripts/UI/HealthBar/HealthBarUIController.cs
@@ -6,15 +6,33 @@ public class HealthBarUIController : MonoBehaviour
    public Image healthBar;
    [SerializeField] private CharStats charStats;
    private Health health;
+   private float previousFill;
+   private float currentFill;
+   private float fillSmoothness = 0.004f;
 
 
    private void Awake()
    {
-      if(charStats == null) charStats = GetComponentInParent<Health>().CharStats;
+      if (charStats == null)
+      {
+         charStats = GetComponentInParent<Health>().CharStats;
+      }
    }
 
    private void LateUpdate()
    {
-      healthBar.fillAmount = charStats.CurrentHealth / charStats.MaxHealth;
+      previousFill = healthBar.fillAmount;
+      currentFill = charStats.CurrentHealth / charStats.MaxHealth;
+   
+      if (currentFill > previousFill)
+      {
+         previousFill = Mathf.Min(previousFill + fillSmoothness, currentFill);
+      }
+      else if (currentFill < previousFill)
+      {
+         previousFill = Mathf.Max(previousFill - fillSmoothness, currentFill);
+      }
+   
+      healthBar.fillAmount = previousFill;
    }
 }
