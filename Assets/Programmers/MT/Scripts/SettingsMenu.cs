936ce9cd72f8b76a543594707ee4f4604b097619
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2021-12-18 14:40:10 +0100

    QualitySettings Done

--- Assets/Programmers/MT/Scripts/SettingsMenu.cs
+++ Assets/Programmers/MT/Scripts/SettingsMenu.cs
@@ -12,4 +12,9 @@ public class SettingsMenu : MonoBehaviour
     {
         audioMixer.SetFloat("volume", volume);
     }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
 }
