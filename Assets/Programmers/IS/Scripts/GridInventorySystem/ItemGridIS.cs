b16c02fafa51daa31b5d6e5c0ecdfb3faaabe0cf
InaSLew <ina.shenlewander@gmail.com>
2021-12-20 15:48:36 +0100

    Item stuck to curosr with one right click

--- Assets/Programmers/IS/Scripts/GridInventorySystem/ItemGridIS.cs
+++ Assets/Programmers/IS/Scripts/GridInventorySystem/ItemGridIS.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 
 /// <summary>
-/// This scriptable object stores configuration info for the grid
-/// and its operation logic.
+/// This scriptable object
+/// (1) stores configuration on the item grid
+/// (2) controls adding and removing logic
 /// </summary>
 
 [CreateAssetMenu(fileName = "inventory grid", menuName = "Game/inventory grid")]
@@ -21,8 +22,9 @@ public class ItemGridIS : ScriptableObject
 
     private InventoryItemIS[,] gridSlots;
     private InventoryItemIS overlapItem;
-    private Vector2 positionOnGrid = new Vector2();
-    private Vector2Int tileGridPosition = new Vector2Int();
+    private InventoryItemIS selectedItem;
+    private Vector2 positionOnGrid;
+    private Vector2Int tileGridPosition;
 
     public void InitGrid()
     {
@@ -38,15 +40,15 @@ public class ItemGridIS : ScriptableObject
         Debug.Log("posY: " + posY);
         Debug.Log("item width: " + itemToAdd.itemData.width);
         Debug.Log("item height: " + itemToAdd.itemData.height);
-        if (IsItemOutsideInventory(posX, posY, itemToAdd.itemData.width, itemToAdd.itemData.height)) return false;
-
-        if (IsItemOverlap(posX, posY, itemToAdd.itemData.width, itemToAdd.itemData.height))
-        {
-            overlapItem = null;
-            return false;
-        }
-
-        if (overlapItem != null) CleanGridReference(overlapItem);
+        
+        // if (IsItemOutsideInventory(posX, posY, itemToAdd.itemData.width, itemToAdd.itemData.height)) return false;
+        //
+        // if (IsItemOverlap(posX, posY, itemToAdd.itemData.width, itemToAdd.itemData.height))
+        // {
+        //     overlapItem = null;
+        //     return false;
+        // }
+        // if (overlapItem != null) CleanGridReference(overlapItem);
         
         var rt = itemToAdd.GetComponent<RectTransform>();
         rt.SetParent(rectTrans);
@@ -71,6 +73,14 @@ public class ItemGridIS : ScriptableObject
         
         return true;
     }
+    
+    private InventoryItemIS RemoveItem(int x, int y)
+    {
+        var toReturn = gridSlots[x, y];
+        if (toReturn == null) return null;
+        CleanGridReference(toReturn);
+        return toReturn;
+    }
 
     private void CleanGridReference(InventoryItemIS item)
     {
@@ -83,74 +93,52 @@ public class ItemGridIS : ScriptableObject
         }
     }
 
-    private bool IsItemOverlap(int posX, int posY, int itemWidth, int itemHeight)
-    {
-        for (var i = 0; i < itemWidth; i++)
-        {
-            for (var j = 0; j < itemHeight; j++)
-            {
-                var targetSlot = gridSlots[posX + i, posY + j];
-                if (targetSlot != null)
-                {
-                    if (overlapItem == null) overlapItem = targetSlot;
-                    else
-                    {
-                        if (overlapItem != targetSlot) return true;
-                    }
-                }
-            }
-        }
-
-        return false;
-    }
-
-    private bool IsItemOutsideInventory(int posX, int posY, int itemWidth, int itemHeight)
-    {
-        if (IsPositionOutsideInventory(posX, posY)) return true;
-        posX += itemWidth - 1;
-        posY += itemHeight - 1;
-        if (IsPositionOutsideInventory(posX, posY)) return true;
-        return false;
-    }
-
-    private bool IsPositionOutsideInventory(int posX, int posY)
-    {
-        if (posX < 0 || posY < 0) return true;
-        if (posX >= width || posY >= height) return true;
-        return false;
-    }
-
+    // private bool IsItemOverlap(int posX, int posY, int itemWidth, int itemHeight)
+    // {
+    //     for (var i = 0; i < itemWidth; i++)
+    //     {
+    //         for (var j = 0; j < itemHeight; j++)
+    //         {
+    //             var targetSlot = gridSlots[posX + i, posY + j];
+    //             if (targetSlot != null)
+    //             {
+    //                 if (overlapItem == null) overlapItem = targetSlot;
+    //                 else
+    //                 {
+    //                     if (overlapItem != targetSlot) return true;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //
+    //     return false;
+    // }
+    //
+    // private bool IsItemOutsideInventory(int posX, int posY, int itemWidth, int itemHeight)
+    // {
+    //     if (IsPositionOutsideInventory(posX, posY)) return true;
+    //     posX += itemWidth - 1;
+    //     posY += itemHeight - 1;
+    //     if (IsPositionOutsideInventory(posX, posY)) return true;
+    //     return false;
+    // }
+    //
+    // private bool IsPositionOutsideInventory(int posX, int posY)
+    // {
+    //     if (posX < 0 || posY < 0) return true;
+    //     if (posX >= width || posY >= height) return true;
+    //     return false;
+    // }
+    //
     internal Vector2Int GetTileGridPosition(Vector2 mousePosition)
     {
         var rectTransPosition = rectTrans.position;
         positionOnGrid.x = mousePosition.x - rectTransPosition.x;
         positionOnGrid.y = rectTransPosition.y - mousePosition.y;
-
+    
         tileGridPosition.x = (int)(positionOnGrid.x / tileSize);
         tileGridPosition.y = (int)(positionOnGrid.y / tileSize);
-
+    
         return tileGridPosition;
     }
-    
-    // private InventoryItemIS PickUpItem(int x, int y)
-    // {
-    //     var toReturn = inventoryItemSlots[x, y];
-    //
-    //     if (toReturn == null) return null;
-    //
-    //     CleanGridReference(toReturn);
-    //     
-    //     return toReturn;
-    // }
-    //
-    // private void CleanGridReference(InventoryItemIS item)
-    // {
-    //     for (int i = 0; i < item.itemData.width; i++)
-    //     {
-    //         for (int j = 0; j < item.itemData.height; j++)
-    //         {
-    //             inventoryItemSlots[item.OnGridPositionX + i, item.OnGridPositionY + j] = null;
-    //         }
-    //     }
-    // }
 }
