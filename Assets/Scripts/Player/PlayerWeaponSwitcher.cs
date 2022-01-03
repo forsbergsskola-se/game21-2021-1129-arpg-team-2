dfecc9c87592adbb57078d42a119d29353e55857
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-01-03 15:29:23 +0100

    Player Can Switch Weapons

---
+++ Assets/Scripts/Player/PlayerWeaponSwitcher.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerWeaponSwitcher : MonoBehaviour
+{
+    [Header("Weapons")]
+    [SerializeField] private Weapon primary;
+    [SerializeField] private Weapon secondary;
+    [SerializeField] private Weapon current;
+
+    private void Awake() => AssignWeapon(primary);
+    
+    private void Update() => SwitchWeapons();
+    
+    private void SwitchWeapons()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) AssignWeapon(primary);
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) AssignWeapon(secondary);
+       
+    }
+
+    private void AssignWeapon(Weapon weapon)
+    {
+        current.Name = weapon.Name;
+        current.Power = weapon.Power;
+        current.Range = weapon.Range;
+        current.Ranged = weapon.Ranged;
+    }
+}
--- Assets/Scripts/SOTemplates/Weapon.cs
+++ Assets/Scripts/SOTemplates/Weapon.cs
@@ -3,10 +3,31 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Weapon", menuName = "Game/Weapon")]
 public class Weapon : ScriptableObject
 {
-    [SerializeField] private new string name;
+    [SerializeField] private string name;
     [SerializeField] private float power;
     [SerializeField] private float range;
-    public float Power => power;
-    public float Range => range;
-    public string Name => name;
+    [SerializeField] private bool ranged;
+    public string Name
+    {
+        get => name;
+        set => name = value;
+    }
+
+    public float Power
+    {
+        get => power;
+        set => power = value;
+    }
+
+    public float Range
+    {
+        get => range;
+        set => range = value;
+    }
+
+    public bool Ranged
+    {
+        get => ranged;
+        set => ranged = value;
+    }
 }
