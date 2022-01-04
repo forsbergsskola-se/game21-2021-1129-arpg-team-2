da26a002c6bed48184f141504e5835c03b28b9b7
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-01-04 11:08:41 +0100

    Player Ranged Attack Spawns Projectiles

---
+++ Assets/Scripts/Player/Shooter.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private Weapon weapon;
+    [SerializeField] private GameObjectValue target;
+    [SerializeField] private Vector3Value shootDirection;
+    
+    public void Shoot()
+    {
+        var temp = Instantiate(weapon.Projectile, transform.position, Quaternion.identity);
+        shootDirection.Vector3 = (transform.position - target.Value.transform.position).normalized;
+    }
+}
