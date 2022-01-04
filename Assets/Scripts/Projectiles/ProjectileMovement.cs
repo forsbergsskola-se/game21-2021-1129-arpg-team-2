b34a0edec9df1d15d6b61abc6b866731c2858963
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-01-04 11:35:44 +0100

    Projectiles deal Damage

---
+++ Assets/Scripts/Projectiles/ProjectileMovement.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class ProjectileMovement : MonoBehaviour
+{
+    [SerializeField] private CharStats playerStats;
+    [SerializeField] private Weapon weapon;
+    [SerializeField] private Vector3Value shootDirection;
+    [SerializeField] private float speed;
+
+    private void Update() => transform.position += shootDirection.Vector3 * speed * Time.deltaTime;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamageable damageable = other.GetComponent<IDamageable>();
+        if(damageable != null) damageable.TakeDamage(weapon.Power * playerStats.AttackSpeed);
+    }
+}
