51368d85ece243f1ba6e71c6e5a6256f8be0f1fd
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2022-01-06 10:17:01 +0100

    LoadForset Script Fix

--- Assets/Scripts/Enviroment/LoadForest.cs
+++ Assets/Scripts/Enviroment/LoadForest.cs
@@ -11,7 +11,7 @@ public class LoadForest : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-            SceneManager.LoadScene(SceneManager.GetSceneByName("ForestNew").buildIndex);
+            SceneManager.LoadScene(2);
         }
     }
 }
