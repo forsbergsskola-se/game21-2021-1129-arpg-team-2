b480ac18ad04fc9a9d4389817708cbc5e8fc3bb7
Max Deurell Kent <max.deurell@gmail.com>
2022-01-07 10:30:24 +0100

    Heal effect pog

--- Assets/Scripts/Health/HealEffect.cs
+++ Assets/Scripts/Health/HealEffect.cs
@@ -6,12 +6,5 @@ public class HealEffect : MonoBehaviour
 {
     [SerializeField] private ParticleSystem healEffect;
 
-    public void PlayEffect() => StartCoroutine(nameof(VisualHealEffect));
-    
-    private IEnumerator VisualHealEffect()
-    {
-        var healParticles = Instantiate(healEffect, transform.position, quaternion.identity);
-        healParticles.Play();
-        yield return new WaitForSeconds(.3f);
-    }
+    public void PlayEffect() => healEffect.Play();
 }
