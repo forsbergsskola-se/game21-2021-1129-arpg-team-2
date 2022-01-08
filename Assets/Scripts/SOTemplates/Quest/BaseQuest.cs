ff54e79f21e3cca5acf73c55f8bfe0a83743ce9c
InaSLew <ina.shenlewander@gmail.com>
2022-01-08 23:21:11 +0100

    Quest log ScriptableObject in place with UI foundation

---
+++ Assets/Scripts/SOTemplates/Quest/BaseQuest.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BaseQuest : ScriptableObject
+{
+    [SerializeField] private string questName;
+    public string QuestName => questName;
+
+    [TextArea(15, 20)]
+    [SerializeField] private string questDescription;
+    public string QuestDescription => questDescription;
+
+    [SerializeField] private List<QuestTodo> questTodoList;
+    public List<QuestTodo> QuestTodoList => questTodoList;
+
+    private QuestStatus questStatus;
+    public QuestStatus QuestStatus => questStatus;
+}
+
+public enum QuestType
+{
+    Main,
+    Side
+}
+
+public enum SubQuestType
+{
+    Collect,
+    Kill
+}
+
+public enum QuestStatus
+{
+    Inactive,
+    Active,
+    Complete
+}
+
+[Serializable]
+public class QuestTodo
+{
+    [SerializeField] private string description;
+    public string Description => description;
+
+    [SerializeField] private bool isComplete;
+    public bool IsComplete { get => isComplete; set => isComplete = value; }
+}
