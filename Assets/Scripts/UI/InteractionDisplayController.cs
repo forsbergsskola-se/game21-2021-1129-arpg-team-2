47a06366de8bb5beb02ff96b798671b5b8f676f9
Armin-AF <armin.afazeli@student.forsbergsskola.se>
2022-01-10 15:11:48 +0100

    Interaction Message

---
+++ Assets/Scripts/UI/InteractionDisplayController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionDisplayController : MonoBehaviour
+{
+    public LayerMask PlayerLayer;
+    public GameObject interactionBox;
+    [SerializeField] private float intractionRange = 15f;
+    private bool isInIntractRange;
+   
+    
+    void Update()
+    {
+        isInIntractRange = Physics.CheckSphere(transform.position, intractionRange, PlayerLayer);
+
+        interactionBox.SetActive(isInIntractRange);
+    }
+}
